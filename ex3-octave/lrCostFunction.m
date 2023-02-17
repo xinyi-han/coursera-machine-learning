66a64a6b18767dab4f8cb49b443f2ba382f8da69
Xinyi Han <xinyi.han98@outlook.com>
2023-02-17 11:54:32 +1100

    Complete ex3

--- ex3-octave/lrCostFunction.m
+++ ex3-octave/lrCostFunction.m
@@ -36,11 +36,11 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
-
-
-
-
-
+h = sigmoid(X * theta);
+J = 1 / m * sum(- y .* log(h) - (1 - y) .* log(1 - h));
+J = J + lambda / (2 * m) * sum(theta(2:end) .^2);
+grad = 1 / m * transpose(sum((h - y) .* X));
+grad = grad + [0; lambda / m * theta(2:end)];
 
 
 
